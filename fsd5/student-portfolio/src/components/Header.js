24676cb634eec3810d07c5d8453ed79033066e4b
Razik Mhate <razikmhate@gmail.com>
2026-04-21 22:43:40 +0530

    Cleaned up assignment folders: renamed to fsd4_5 and removed duplicates

--- fsd5/student-portfolio/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="portfolio-header">
-      <h1>Razik Mhate</h1>
-      <p>3rd-Year Undergraduate Engineering Student | AI & Full-Stack Developer</p>
-    </header>
-  );
-}
-export default Header;
